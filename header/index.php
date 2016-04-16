a9afb68ca396ca116f32a8469bb81bbe0a4eb1f8
johnny20 <giovanny.johnny.20@gmail.com>
2016-04-16 20:36:49 +0300

    login-register

--- header/index.php
+++ header/index.php
@@ -8,9 +8,63 @@
 </head>
 <body>
 
-	<?php
-
-	echo "<br/><h2>Hello this is header</h2>";
-	
-	?>
-
+<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="/Home">Home</a>
+				</div>
+				<div class="collapse navbar-collapse" id="myNavbar">
+					<ul class="nav navbar-nav">
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Calendar<span class="caret"></span></a>
+							<ul class="dropdown-menu">
+								<li>
+									<a href="/AddReminder">Add Reminder</a>
+								</li>
+								<li>
+									<a href="/RemoveReminder">Remove Reminder</a>
+								</li>
+							</ul>
+						</li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Todo's <span class="caret"></span></a>
+							<ul class="dropdown-menu">
+								<li>
+									<a href="/AddTask">Add Task</a>
+								</li>
+								<li>
+									<a href="/RemoveTask">Remove Task</a>
+								</li>
+							</ul>
+						</li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Achievements <span class="caret"></span></a>
+							<ul class="dropdown-menu">
+								<li>
+									<a href="/AddAchievement">Add Achievements</a>
+								</li>
+								<li>
+									<a href="/RemoveAchievement">Remove Achievements</a>
+								</li>
+							</ul>
+						</li>
+						<li class="active">
+							<a href="/About">About</a>
+						</li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<li>
+							<a href="/Login"><span class="glyphicon glyphicon-log-in" ></span> Login</a>
+						</li>
+						<li class="hidden">
+							<a href="/Logout"><span class="glyphicon glyphicon-log-out" ></span> Logout</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
---
+++ register/dao/index.php
@@ -0,0 +1,117 @@
+<?php
+$basedir = realpath(__DIR__);
+include($basedir . '/db_connection/mysqli_connect.php');
+function input($data){
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+$username=$password=$email=$phone="";
+$requsername=$reqpassword=$reqemail=$reqphone="";
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+	
+	//Define select queries
+	$query = "SELECT * FROM users;";
+	$response = mysqli_query($dbc, $query);
+	/* while($row = mysqli_fetch_array($response)){
+		//edw perneis tous users 
+	} */
+		
+	//mysqli_fetch_assoc();
+	
+	
+	//Metablites...
+	//$username = input($_POST["username"]);
+	$email = input($_POST["txtUserEmail"]);
+	//$password = input($_POST["txtUserPass"]);
+	$phone = input($_POST["phone"]);
+
+	//invalid 
+		if(empty($_POST["username"])){
+			$requsername = "You have to put a username";
+			echo $requsername."<br>";
+		}
+		else{
+			$username = input($_POST["username"]);
+			
+			if($response->num_rows > 0){
+				while($row = $response->fetch_assoc()){
+					if($row["username"] == $username){
+						$requsername = "This username already exists";	
+						echo $requsername."<br>";
+					}
+				}
+			}
+		}
+		if(empty($_POST["password"]) || empty($_POST["passwordconf"])){
+			$reqpassword = "You have to put a password";
+			echo $reqpassword."<br>";
+		}
+			else if($_POST["password"] != $_POST["passwordconf"] ){
+			$reqpassword = "Your password wasn't confirm";
+			echo $reqpassword."<br>";
+			}
+		else
+			$password = input($_POST["txtUserPass"]);
+		
+		
+		if(empty($_POST["phone"]) || !is_numeric($_POST["phone"]) ){
+			$reqphone = "You have to put a phone";
+			echo $reqphone."<br>";
+		}		
+		else{
+			if($response->num_rows > 0){
+					while($row = $response->fetch_assoc()){
+						if($row["phone"] == $phone){
+							$reqphone = "This phone already exists";
+							echo $reqphone."<br>";
+						}
+					}
+				}
+		}
+		
+		if(empty($_POST["email"])){
+			$reqemail = "You have to put a email";
+			echo $reqemail."<br>";
+		}
+		else{
+			if($response->num_rows > 0){
+					while($row = $response->fetch_assoc()){
+						if($row["email"] == $email){
+							$reqemail = "This email already exists";
+							echo $reqemail."<br>";
+						}
+					}
+				}
+		}
+		//Define insert query
+		$new_user_query = "INSERT INTO users(username,password,email,phone,rank) VALUES ('".$username."','".$password."','".$email."','".$phone."','0');";
+		$new_user_response = mysqli_query($dbc, $new_user_query); 
+		
+		if($requsername =="" && $reqpassword=="" && $reqemail =="" && $reqphone==""){
+			if($new_user_response == true)
+				echo "New record created successfully";
+			else
+				echo "Error: " .$new_user_query. "<br>" . $dbc->error;	
+		}
+		
+}
+
+?>
+
+<html>
+<head>
+</head>
+<body>
+<form action="<?php echo ($_SERVER["PHP_SELF"]);?>" method="post">
+<input type="text" name="username"><br>
+<input type="password" name="password"><br>
+<input type="password" name="passwordconf"><br>
+<input type="text" name="email"><br>
+<input type="text" name="phone"><br>
+<input type="submit">
+</form>
+</body>
+</html>
