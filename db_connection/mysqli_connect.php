564419bf3472b5241b2b6ff0630286e2121ab83a
Zarchon <panagiwtis.geor@gmail.com>
2016-04-16 15:00:48 +0300

    basic achievements issue structure with some basic connections to the database

--- db_connection/mysqli_connect.php
+++ db_connection/mysqli_connect.php
@@ -1,17 +1,15 @@
 <?php
 
-	define('DB_USER', 'root');
-	define('DB_PASSWORD', '');
-	define('DB_HOST', 'localhost');
-	define('DB_NAME', 'alvanoudis_clients');
+	define('DB_USER', 'connect');
+	define('DB_PASSWORD', '123');
+	define('DB_HOST', '83.212.99.34');
+	define('DB_NAME', 'hackathon');
 
 	$dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die('Could not connect to mySQL' . mysqli_connect_error());
 
-	mysqli_query($dbc,"SET NAMES 'utf8'");
-	mysqli_query($dbc,"SET CHARACTER SET 'utf8'");
+	//mysqli_query($dbc,"SET NAMES 'utf8'");
+	//mysqli_query($dbc,"SET CHARACTER SET 'utf8'");
 	
-	//$dbc->close();
-	
-
+	//$dbc->close();	
 
 ?>
